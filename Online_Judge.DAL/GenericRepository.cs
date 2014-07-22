0258f2688baf510972b5d5041274c19f76dd3739
sobiczew <grzegorz.sobiczewski@gmail.com>
2014-07-22 23:03:58 +0200

    DEV: Implemented GenericRepository

---
+++ Online_Judge.DAL/GenericRepository.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using Online_Judge.DAL.Specification;
+
+namespace Online_Judge.DAL
+{
+	/// <summary>
+	/// GenericRepository class
+	/// </summary>
+	public class GenericRepository : IRepository
+	{
+		#region Dependencies
+
+		private readonly DbContext _context;
+
+		#endregion
+
+		#region Fields
+
+		private IUnitOfWork _unitOfWork;
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GenericRepository"/> class.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <exception cref="System.ArgumentNullException">context</exception>
+		public GenericRepository(DbContext context)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+
+			_context = context;
+		}
+
+		#endregion
+
+		#region IRepository Members
+
+		public IUnitOfWork UnitOfWork
+		{
+			get { return _unitOfWork ?? (_unitOfWork = new UnitOfWork(_context)); }
+		}
+
+		public IQueryable<TEntity> GetQuery<TEntity>() where TEntity : class
+		{
+			var entityName = GetEntityName<TEntity>();
+			return ((IObjectContextAdapter) _context).ObjectContext.CreateQuery<TEntity>(entityName);
+		}
+
+		public IQueryable<TEntity> GetQuery<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>());
+		}
+
+		public IEnumerable<TEntity> Get<TEntity, TOrderBy>(ISpecification<TEntity> specification,
+		                                                   Expression<Func<TEntity, TOrderBy>> orderBy, int pageIndex,
+		                                                   int pageSize,
+		                                                   SortOrder sortOrder = SortOrder.Ascending) where TEntity : class
+		{
+			if (sortOrder == SortOrder.Ascending)
+			{
+				return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>())
+									.OrderBy(orderBy)
+									.Skip((pageIndex - 1)*pageSize)
+									.Take(pageSize)
+									.AsEnumerable();
+			}
+
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>())
+								.OrderByDescending(orderBy)
+								.Skip((pageIndex - 1)*pageSize)
+								.Take(pageSize)
+								.AsEnumerable();
+		}
+
+		public IEnumerable<TEntity> GetAll<TEntity>() where TEntity : class
+		{
+			return GetQuery<TEntity>().AsEnumerable();
+		}
+
+		public TEntity Single<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>()).Single();
+		}
+
+		public TEntity SingleOrDefault<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>()).SingleOrDefault();
+		}
+
+		public TEntity First<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>()).First();
+		}
+
+		public TEntity FirstOrDefault<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>()).FirstOrDefault();
+		}
+
+		public IEnumerable<TEntity> Find<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>()).AsEnumerable();
+		}
+
+		public void Add<TEntity>(TEntity entity) where TEntity : class
+		{
+			if (entity == null)
+			{
+				throw new ArgumentNullException("entity");
+			}
+
+			_context.Set<TEntity>().Add(entity);
+		}
+
+		public void Add<TEntity>(IEnumerable<TEntity> entities) where TEntity : class
+		{
+			if (entities == null || !entities.Any())
+			{
+				throw new ArgumentNullException("entities");
+			}
+
+			_context.Set<TEntity>().AddRange(entities);
+		}
+
+		public void Delete<TEntity>(TEntity entity) where TEntity : class
+		{
+			if (entity == null)
+			{
+				throw new ArgumentNullException("entity");
+			}
+
+			_context.Set<TEntity>().Remove(entity);
+		}
+
+		public void Delete<TEntity>(IEnumerable<TEntity> entities) where TEntity : class
+		{
+			if (entities == null || !entities.Any())
+			{
+				throw new ArgumentNullException("entities");
+			}
+
+			_context.Set<TEntity>().RemoveRange(entities);
+		}
+
+		public void Delete<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			IEnumerable<TEntity> records = Find(specification);
+			_context.Set<TEntity>().RemoveRange(records);
+		}
+
+		public int Count<TEntity>() where TEntity : class
+		{
+			return GetQuery<TEntity>().Count();
+		}
+
+		public int Count<TEntity>(ISpecification<TEntity> specification) where TEntity : class
+		{
+			return specification.SatisfyingEntitiesFrom(GetQuery<TEntity>()).Count();
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private string GetEntityName<TEntity>() where TEntity : class
+		{
+			string entitySetName = ((IObjectContextAdapter) _context).ObjectContext
+			                                                         .MetadataWorkspace
+			                                                         .GetEntityContainer(
+				                                                         ((IObjectContextAdapter) _context).ObjectContext
+				                                                                                           .DefaultContainerName,
+				                                                         DataSpace.CSpace)
+			                                                         .BaseEntitySets.First(
+				                                                         bes => bes.ElementType.Name == typeof (TEntity).Name).Name;
+
+			return string.Format("{0}.{1}", ((IObjectContextAdapter) _context).ObjectContext.DefaultContainerName, entitySetName);
+		}
+
+		#endregion
+	}
+}
--- Online_Judge.DAL/IRepository.cs
+++ Online_Judge.DAL/IRepository.cs
@@ -1,10 +1,146 @@
-﻿namespace Online_Judge.DAL
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using Online_Judge.DAL.Specification;
+
+namespace Online_Judge.DAL
 {
 	/// <summary>
 	/// IRepository interface
 	/// </summary>
 	public interface IRepository
 	{
+		/// <summary>
+		/// Gets the query.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <returns></returns>
+		IQueryable<TEntity> GetQuery<TEntity>() where TEntity : class;
 
+		/// <summary>
+		/// Gets the query.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		IQueryable<TEntity> GetQuery<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Gets single entity using specification
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		TEntity Single<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Singles the or default.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		TEntity SingleOrDefault<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Gets first entity with specification.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		TEntity First<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Firsts the or default.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		TEntity FirstOrDefault<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Adds the specified entity.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="entity">The entity.</param>
+		void Add<TEntity>(TEntity entity) where TEntity : class;
+
+		/// <summary>
+		/// Adds the specified entities.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="entities">The entities.</param>
+		void Add<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
+
+		/// <summary>
+		/// Deletes the specified entity.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="entity">The entity.</param>
+		void Delete<TEntity>(TEntity entity) where TEntity : class;
+
+		/// <summary>
+		/// Deletes the specified entities.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="entities">The entities.</param>
+		void Delete<TEntity>(IEnumerable<TEntity> entities) where TEntity : class;
+
+		/// <summary>
+		/// Deletes entities which satify specificatiion
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		void Delete<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Finds entities based on provided criteria.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		IEnumerable<TEntity> Find<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Gets all.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <returns></returns>
+		IEnumerable<TEntity> GetAll<TEntity>() where TEntity : class;
+
+		/// <summary>
+		/// Gets entities which satifies a specification.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <typeparam name="TOrderBy">The type of the order by.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <param name="orderBy">The order by.</param>
+		/// <param name="pageIndex">Index of the page.</param>
+		/// <param name="pageSize">Size of the page.</param>
+		/// <param name="sortOrder">The sort order.</param>
+		/// <returns></returns>
+		IEnumerable<TEntity> Get<TEntity, TOrderBy>(ISpecification<TEntity> specification, Expression<Func<TEntity, TOrderBy>> orderBy, int pageIndex, int pageSize, SortOrder sortOrder = SortOrder.Ascending) where TEntity : class;
+
+		/// <summary>
+		/// Counts the specified entities.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <returns></returns>
+		int Count<TEntity>() where TEntity : class;
+
+		/// <summary>
+		/// Counts entities satifying specification.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="specification">The specification.</param>
+		/// <returns></returns>
+		int Count<TEntity>(ISpecification<TEntity> specification) where TEntity : class;
+
+		/// <summary>
+		/// Gets the unit of work.
+		/// </summary>
+		/// <value>The unit of work.</value>
+		IUnitOfWork UnitOfWork { get; }
 	}
 }
